8dc51d286dd9622247c6364f1b306cd86e38ae21
hunglnl01 <hunglnl01@gmail.com>
2020-04-05 04:35:42 -0700

    changes to hamilton algo

--- Hamiltonian.py
+++ Hamiltonian.py
@@ -1,12 +1,13 @@
-from Graph import *
+from Graph import Graph
 
 def hamilton(G, source, end, path = []):
+    # if the vertex id is not yet in the path, add it to path
     if source not in set(path):
         path.append(source)
 
         # if the path is graph.size() and last element is 'end'
         if(len(path) == G.size() and path[G.size()-1] == end):
-            print('hamilton path: {}' .format(path))
+            print('hamilton path: {}' .format(path)) # find a path and print it
         
         # source.neighbor doesnt contain 'end'
         if end not in G.vertices[source].neighbor:
@@ -25,21 +26,3 @@ def hamilton(G, source, end, path = []):
             # lastly do the case of 'end'
             res_path = [i for i in path]
             hamilton(G, end, end, res_path)
-                
-
-# generate a graph with 6 vertices
-G = Graph()
-for i in range(0,6,1):
-    G.add_vertex(Vertex(i))
-
-edges = [(0,2), (1,2), (0,5), (2,3), (3,5), (3,4), (1,3), (4,5), (0,1), (1,4), (2,4)]
-for edge in edges:
-    G.add_edge(edge[0], edge[1])
-
-# testing
-G.printGraph()
-path = []
-sourceID = 0
-endID = 5
-hamilton(G, sourceID, endID, path)
-
